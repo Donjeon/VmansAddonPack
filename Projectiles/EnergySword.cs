a5835aa5bde5d3e3c8b9ffa79fa4e78241698009
Don <56922545+Donjeon@users.noreply.github.com>
2020-02-01 15:54:30 +0000

    New projectile functionality
    The sword fired is now "a sword of chaotic energy that chases after enemies and strikes multiple times". Ideas:
    -Have a basic ammo type that does not pierce
    -Have a more advanced ammo type that does pierce
    -Have a super powered type that pierces, ignores tiles and has a gigantic homing range

--- Projectiles/EnergySword.cs
+++ Projectiles/EnergySword.cs
@@ -15,6 +15,8 @@ namespace VmansAddonPack.Projectiles
             Main.projFrames[projectile.type] = 3; //show the projectile has 3 animation frames
         }
 
+        //Fires a sword of chaotic energy that chases after enemies and strikes multiple times
+
         public override void SetDefaults()
         {
             projectile.width = 10;
@@ -23,17 +25,24 @@ namespace VmansAddonPack.Projectiles
             projectile.friendly = true;
             projectile.ranged = true;
             projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(45f);
+            projectile.noDropItem = true;
+
+            projectile.penetrate = Main.rand.Next(2, 8); //these 2 lines along with homing make for a very cool effect
+            projectile.tileCollide = true; //false
+
+
             aiType = 156;
 
-            /*
+            
             drawOffsetX = -30; //Hitbox begins at tip. This seems correct
             drawOriginOffsetX = 16;
             drawOriginOffsetY = 0;
-            */
-
+            
+            /*
             drawOffsetX = -6; //Corrects offset when being shot. Hitbox begins at the hilt, instead of the tip
             drawOriginOffsetX = -12;
             drawOriginOffsetY = -27;
+            */
 
 
         }
@@ -42,6 +51,8 @@ namespace VmansAddonPack.Projectiles
 
         public override void AI()
         {
+
+          
 
 
             projectile.ai[0] += 1f; //how many pixels it has travelled????
@@ -65,16 +76,18 @@ namespace VmansAddonPack.Projectiles
             }
 
 
-            if (true) //always the case, done so i can see the stuff
+            if (projectile.active) //always the case, done so i can see the stuff
             {
                 Lighting.AddLight(projectile.Center, 0.6f, 0.2f, 1f); // R G B values from 0 to 1f. This makes purple of #9933ff
-                Dust.NewDust(projectile.position, projectile.width, projectile.height, 21, projectile.velocity.X * 0.25f, projectile.velocity.Y * 0.25f, 150, default(Color), 0.7f);
+
+                for(int i = 0; i < 2; i++) //i < number of times the particles should be generated per cycle
+                {
+                    Dust.NewDust(projectile.position, projectile.width, projectile.height, 21, projectile.velocity.X * 0.25f, projectile.velocity.Y * 0.25f, 150, default(Color), 0.7f);
+                }
+                
             }
-
-            float num167 = projectile.position.X;
-            float num168 = projectile.position.Y;
-
-            if (true) //if the projectile exists
+            
+            if (projectile.active) //if the projectile exists
             {
 
 
@@ -82,7 +95,7 @@ namespace VmansAddonPack.Projectiles
                 {
                     NPC target = Main.npc[i];
 
-                    if (target.CanBeChasedBy()) //if the target can be chased by this projectile
+                    if (target.CanBeChasedBy() && projectile.alpha > 100) //if the target can be chased by this projectile
                     {
                         //Get the shoot trajectory from the projectile and target
                         float shootToX = target.position.X + (float)target.width * 0.5f - projectile.Center.X;
@@ -91,10 +104,11 @@ namespace VmansAddonPack.Projectiles
                         float Num = Math.Abs(projectile.position.X + (float)(projectile.width / 2) - shootToX) + Math.Abs(projectile.position.Y + (float)(projectile.height / 2) - shootToY); //AI taken from chlorophyte and adapted, Projectile.cs, line 31413
 
                         float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
+                        
 
 
-                        //If the distance between the live targeted npc and the projectile is less than 240 pixels
-                        if (distance < 240f && target.active && Collision.CanHit(new Vector2(projectile.position.X + (float)(projectile.width / 2), projectile.position.Y + (float)(projectile.height / 2)), 1, 1, target.position, target.width, target.height))
+                        //If the distance between the live targeted npc. Distance is chosen to be a random distance between 50 and 500
+                        if (distance < Main.rand.Next(50, 500) && target.active && Collision.CanHit(new Vector2(projectile.position.X + (float)(projectile.width / 2), projectile.position.Y + (float)(projectile.height / 2)), 1, 1, target.position, target.width, target.height))
                         {
                             //Divide the factor, 3f, which is the desired velocity
                             distance = 3f / distance;
@@ -115,13 +129,18 @@ namespace VmansAddonPack.Projectiles
             }
 
         }
-        /*
-        public void Kill() //Do something when it dies
+
+        
+        
+        public override void Kill(int timeleft) //Do something when it dies
         {
-            Main.PlaySound(SoundID.Item25, projectile.position);
+            //Main.PlaySound(SoundID.Item25, projectile.position);
+            Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 10); //Main.PlaySound(int listid, int x, int y, int soundid) first number is the list, second is the style.
             Dust.NewDust(projectile.position, projectile.width, projectile.height, 27, projectile.velocity.X * 0.25f, projectile.velocity.Y * 0.25f, 150, default(Color), 0.7f);
+
+
         }
         //TODO: Get a proper sound to play on death and an explosion like jester arrows
-        */
+        
     }
 }
